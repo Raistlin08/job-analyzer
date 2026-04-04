7ea36f6c2705724eff1ca20ac14b79ff1747243e
Tommaso Vicario <tommaso.vicario@gmail.com>
2026-04-04 10:38:19 +0200

    Decided standard response format

--- server/app.py
+++ server/app.py
@@ -3,6 +3,13 @@ from flask import Flask, send_file, jsonify, request
 app = Flask(__name__)
 
 
+standard_response = {
+    "overview": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rhoncus orci vel ante vehicula, ut sollicitudin tellus condimentum. Sed tortor velit, molestie ac tortor vel, tincidunt rutrum nunc. Duis egestas, mi sed tempus convallis, augue leo vestibulum tortor, in sagittis nulla velit at est. Sed cursus nulla odio, et lacinia.",
+    "details": [
+        {"skill": "C++","relevance":1,"difficulty":3},
+        {"skill": "docker","relevance":2,"difficulty":4}
+    ]
+}
 
 
 @app.route('/')
@@ -16,7 +23,8 @@ def analyze():
     data = request.get_json()
     print(data["job"])
     print(data["location"])
-    return "sds"
+    
+    return jsonify(standard_response)
 
 if __name__ == '__main__':
     app.run(debug=True)
